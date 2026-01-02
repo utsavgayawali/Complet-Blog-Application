a40065d85351eae271b77220b6a78a9ebcdb8c56
Utsab Gayawali <utsavgayawali@gmail.com>
2026-01-02 17:28:52 +0545

    admin pannel and models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+admin.site.register(Post)
+
+
+# blog
+# blog
+
--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from  django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=200)
+    time = models.DateTimeField(default=timezone.now)
+    auther = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
