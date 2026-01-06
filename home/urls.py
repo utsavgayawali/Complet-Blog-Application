5a3c52b16e8d82f3866057d660caef67d624b500
Utsab Gayawali <utsavgayawali@gmail.com>
2026-01-06 16:45:56 +0545

    adding user templates and views

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,12 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
-    path('about/', views.About, name='about')
+    path('about/', views.About, name='about'),
+
+
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    return render(request,'Base/home.html')
+
+    context={
+        'post':Post.objects.all()
+    }
+    return render(request,'Base/home.html',context)
+
+
 
 def About(request):
     return render(request,'Base/about.html')
+
+
+def register_view(request):
+    return render(request,'User/register.html')
+def login_view(request):
+    return render(request,'User/login.html')
+def logout_view(request):
+    return render(request,'User/logout.html')
+def profile_view(request):
+    return render(request,'User/profile.html')
